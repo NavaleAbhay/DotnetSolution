f6b6f31fa0977e9fcf4919e75b32df8405af4d76
Abhaynavale-6080 <navale.abhay12@gmail.com>
2022-12-08 14:58:16 +0530

    c# basics

---
+++ CRMLib/Customer.cs
@@ -0,0 +1,32 @@
+﻿namespace CRMLib;
+public class Customer
+{
+public int Id{get;set;}
+public string FirstName{get;set;}
+public string LastName{get;set;}
+public long ContactNumber{get;set;}
+public string Email{get;set;}
+
+public Customer(){
+    this.Id=1;
+this.FirstName="Abhay";
+this.LastName="Navale";
+this.ContactNumber=9075966080;
+this.Email="navale.abhay12@gmail.com";
+}
+
+public Customer(int id,string firstname,string lastname,long monumber,string em){
+    this.Id=id;
+    this.FirstName=firstname;
+    this.LastName=lastname;
+    this.ContactNumber=monumber;
+    this.Email=em;
+}
+
+    public override string ToString()
+    {
+        return this.Id+"***"+this.FirstName+"***"+this.LastName+"***"+this.ContactNumber+"***"+this.Email;
+    }
+
+
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using CatalogLib;
+using CRMLib;
+using OrderProcessingLib;
+
+Product p1=new Product();
+p1.Id=12;
+p1.Title="Samsung guru";
+p1.Description="The cheepest mobile phone";
+p1.UnitPrice=999;
+p1.StockAvailable=20000;
+p1.ImageUrl="https://www.vivo.com/in/products/x80pro?_ga=2.139308912.2108742531.1670485026-1397980863.1670485026";
+
+Console.WriteLine(p1.Id+"___"+p1.Title+"___"+p1.Description+"___"+p1.UnitPrice+"___"+p1.StockAvailable+"___"+p1.ImageUrl);
+
+Customer c1=new Customer();
+Customer c2=new Customer(2,"Sahil","Mankar",9878767869,"sahilmankar12@gmail.com");
+Console.WriteLine(c1);
+Console.WriteLine(c2);
+
+Order o1=new Order();
+o1.OrderId=11;
+o1.OrderDate=new DateTime(2022,05,09);
+o1.Customer="Microsoft";
+o1.TotalAmount=20000;
+o1.Status="shipped";
+Console.WriteLine(o1.OrderId+"***"+o1.OrderDate+"***"+o1.Customer+"***"+o1.TotalAmount+"***"+o1.Status);
+
